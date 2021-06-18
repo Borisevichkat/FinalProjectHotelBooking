3cb54e2af6a1cbaef6e68443dd303ab3c7c708cc
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-06-18 10:22:45 +0300

    Add files via upload

---
+++ main/java/by/borisevich/hotel/controller/PageRequest.java
@@ -0,0 +1,8 @@
+package by.borisevich.hotel.controller;
+
+public class PageRequest {
+    public final static String MAIN_PAGE = "/WEB-INF/jsp/main.jsp";
+    public final static String GO_TO_LOGIN_PAGE="";
+    public final static String REGISTRATION = "/WEB-INF/jsp/registration.jsp";
+    public final static String AUTHORISATION = "/WEB-INF/jsp/authorisation.jsp";
+}
